85640f5841ed1199c8b1ba83b2f87113477b539e
JohnMeistrell <52321750+JohnMeistrell@users.noreply.github.com>
2024-07-10 19:27:29 -0400

    Add files via upload

---
+++ js/mapPoints.js
@@ -0,0 +1,12 @@
+const peopleArrays = [
+  /*                        LeftRight   UpDown         LeftRight    UpDown                           */
+  [  'Video#' , 'Photo#' , 'WhereR'  , 'WhereC(px)' , 'WhatMagR'  , 'WhatMagC' , 'Title'],   
+  [  '01'     , '01a'    , '56%'   ,  '130px'       , '55%'       , '58%'      ,'1a - Love'   ], /*GOOD*/
+  [  '01'     , '01a'    , '20%'   ,   '60px'       , '28%'       , '32%'      ,'2a - Museum' ], /*GOOD*/
+  [  '01'     , '01a'    , '21%'   ,   '57px'       , '30%'       , '35%'      ,'3a - George' ], /*GOOD*/
+  [  '01'     , '01a'    , '21%'   ,   '61px'       , '29%'       , '33.5%'    ,'4 - Rocky'   ], /*GOOD*/
+  [  '01'     , '01a'    , '21%'   ,   '61px'       , '29%'       , '33.5%'    ,'5 - Rocky'   ], /*GOOD*/
+  [  '01'     , '01a'    , '21%'   ,   '61px'       , '29%'       , '33.5%'    ,'6 - Rocky'   ], /*GOOD*/
+  [  '01'     , '01a'    , '89%'   ,  '175px'       , '79%'       , '65%'      ,'7a - Bell'   ], /*GOOD*/
+  [  '01'     , '01a'    , '66%'   ,  '130px'       , '65%'       , '58%'      ,'8a - Market' ]  /*GOOD  hard rock very close down left */
+];
