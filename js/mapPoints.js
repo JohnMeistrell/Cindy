3228307c14632aed5fa5fdc3c268b7b411f0d15b
JohnMeistrell <52321750+JohnMeistrell@users.noreply.github.com>
2024-07-13 15:57:36 -0400

    Add files via upload

--- js/mapPoints.js
+++ js/mapPoints.js
@@ -1,12 +1,17 @@
-const peopleArrays = [
-  /*                        LeftRight   UpDown         LeftRight    UpDown                           */
-  [  'Video#' , 'Photo#' , 'WhereR'  , 'WhereC(px)' , 'WhatMagR'  , 'WhatMagC' , 'Title'],   
-  [  '01'     , '01a'    , '56%'   ,  '130px'       , '55%'       , '58%'      ,'1a - Love'   ], /*GOOD*/
-  [  '01'     , '01a'    , '20%'   ,   '60px'       , '28%'       , '32%'      ,'2a - Museum' ], /*GOOD*/
-  [  '01'     , '01a'    , '21%'   ,   '57px'       , '30%'       , '35%'      ,'3a - George' ], /*GOOD*/
-  [  '01'     , '01a'    , '21%'   ,   '61px'       , '29%'       , '33.5%'    ,'4 - Rocky'   ], /*GOOD*/
-  [  '01'     , '01a'    , '21%'   ,   '61px'       , '29%'       , '33.5%'    ,'5 - Rocky'   ], /*GOOD*/
-  [  '01'     , '01a'    , '21%'   ,   '61px'       , '29%'       , '33.5%'    ,'6 - Rocky'   ], /*GOOD*/
-  [  '01'     , '01a'    , '89%'   ,  '175px'       , '79%'       , '65%'      ,'7a - Bell'   ], /*GOOD*/
-  [  '01'     , '01a'    , '66%'   ,  '130px'       , '65%'       , '58%'      ,'8a - Market' ]  /*GOOD  hard rock very close down left */
+const mapMediaInfo = [
+/*                        LeftRight   UpDown         LeftRight    UpDown                      MAP          MAP     */
+['Video#', 'Photo#', 'WhereR', 'WhereCpx', 'WhatMagR%', 'WhatMagC%', 'VidRowLR', 'VidColUP', 'MapRowLR' , 'MapColUD' , 'PicRowLR%', 'PicColUPpx', 'Title'      ],   
+['01a'   , '01'    , '56%'   , '130px'   , '55%'      , '58%'      , '300'     , '200'     ,   '5'      ,   '7'      ,  '20'      ,   '1'       , '1a - Love'   ], /*GOOD*/
+['02a'   , '02'    , '20%'   ,  '60px'   , '28%'      , '32%'      , '50%'     , '80px'    ,   '5'      ,   '7'      , '130'      , '200'       , '2a - Museum' ], /*GOOD*/
+
+['03a'   , '03'    , '21%'   ,  '57px'   , '30%'      , '35%'      , '50%'     , '80px'    ,   '5'      ,   '7'      , '140'      ,  '65'       , '3a - George' ], /*GOOD*/
+['04'    , '04'    , '21%'   ,  '61px'   , '29%'      , '33.5%'    , '50%'     , '80px'    ,   '5'      ,   '7'      , '130'      , '100'       , '4 - Rocky'   ], /*GOOD*/
+
+['05'    , '05'    , '21%'   ,  '61px'   , '29%'      , '33.5%'    , '50%'     , '80px'    ,   '5'      ,   '7'      ,  '80'      , '120'       , '5 - Rocky'   ], /*GOOD*/
+['06'    , '06'    , '21%'   ,  '61px'   , '29%'      , '33.5%'    , '50%'     , '80px'    ,   '5'      ,   '7'      , '300'      , '120'       , '6 - Rocky'   ], /*GOOD*/
+
+['07a'   , '07'    , '89%'   , '175px'   , '79%'      , '65%'      , '50%'     , '80px'    , '200'      ,   '7'      , '180'      , '120'       , '7a - Bell'   ], /*GOOD*/
+['08a'   , '08'    , '66%'   , '130px'   , '65%'      , '58%'      , '50%'     , '80px'    , '200'      ,   '7'      , '200'      , '400'       , '8a - Market' ]  /*GOOD  hard rock very close down left */
 ];
+
+
